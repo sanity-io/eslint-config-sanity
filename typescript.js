73a95f8047c6b048b0ccdab7f75e59cc6910d7e7
Taylor Beseda <tbeseda@gmail.com>
2024-10-22 12:48:09 -0600

    feat: ESLint v9 flat config compat
    zero "deprecated" or missing rules
    BREAKING CHANGE: not compatible with eslintrc

--- typescript.js
+++ typescript.js
@@ -1,47 +1,39 @@
+import tseslint from 'typescript-eslint'
+
 // Use this for TypeScript projects
 // If using both TypeScript + React:
-// make sure you load `sanity/react` _before_  `sanity/typescript`
-module.exports = {
-  extends: [
-    './index',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  env: {
-    node: true,
-    browser: true,
-  },
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
-  rules: {
-    // --- causing parse errors with d.ts files
-    // --- see https://github.com/typescript-eslint/typescript-eslint/issues/420
-    'no-useless-constructor': 'off',
-    '@typescript-eslint/no-useless-constructor': 'error',
-    // ---
+// make sure you load `sanity/react` _before_ `sanity/typescript`
 
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      // --- causing parse errors with d.ts files
+      // --- see https://github.com/typescript-eslint/typescript-eslint/issues/420
+      'no-useless-constructor': 'off',
+      '@typescript-eslint/no-useless-constructor': 'error',
+      // ---
 
-    // does not understand typescript types/interfaces defined late
-    'no-use-before-define': 'off',
+      // set to warn below in old "overrides" section
+      // '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      // '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
 
-    // prevents function overloading in typescript, so disable base rule
-    'no-redeclare': 'off',
-    '@typescript-eslint/no-redeclare': ['error'],
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        // Enable for TS files, but allow TSX (eg react components)
-        '@typescript-eslint/explicit-module-boundary-types': ['warn'],
-        // These are better handled by TypeScript
-        'no-undef': 'off',
-        'no-undef-init': 'off',
-      },
-    },
-  ],
-}
+      // does not understand typescript types/interfaces defined late
+      'no-use-before-define': 'off',
+
+      // prevents function overloading in typescript, so disable base rule
+      'no-redeclare': 'off',
+      '@typescript-eslint/no-redeclare': ['error'],
+
+      // overrides:
+      // Enable for TS files, but allow TSX (eg react components)
+      '@typescript-eslint/explicit-module-boundary-types': ['warn'],
+      // These are better handled by TypeScript
+      'no-undef': 'off',
+      'no-undef-init': 'off',
+    }
+  }
+)
